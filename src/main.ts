2fa3aa2a319e9853b1e7f394b7c71b2ecde28fb3
hsu1006 <hsuwy011006@gmail.com>
2022-11-22 14:59:21 +0800

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const configService = app.get<ConfigService>(ConfigService);
 
   const microserviceTCP = app.connectMicroservice<MicroserviceOptions>({
